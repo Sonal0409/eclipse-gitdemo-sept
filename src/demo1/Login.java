aa7368aa05bfa2502d4d3c82b1129f714b8f1210
sonal0409 <mittal.sonal04@gmail.com>
2023-09-03 10:13:41 +0530

    Changes to be commited

---
+++ src/demo1/Login.java
@@ -0,0 +1,13 @@
+package demo1;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello to github");
+		System.out.println("Hello to git");
+
+	}
+
+}
---
+++ src/demo1/Logout.java
@@ -0,0 +1,12 @@
+package demo1;
+
+public class Logout {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is code for logout");
+
+	}
+
+}
