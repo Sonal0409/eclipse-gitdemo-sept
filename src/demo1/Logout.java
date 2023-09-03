c3419681c2070e728108440ff0584e0abcf45fd4
sonal0409 <mittal.sonal04@gmail.com>
2023-09-03 10:32:16 +0530

    message

--- src/demo1/Logout.java
+++ src/demo1/Logout.java
@@ -4,7 +4,7 @@ public class Logout {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("this is code for logout");
 		System.out.println("this is code for logout");
 
 	}
